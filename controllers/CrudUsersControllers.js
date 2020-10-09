865bd6c3aea881a72c804155f38a87ba33a0e8ed
Thiago Puyelli <thiagopuyelli@pop-os.localdomain>
2020-10-09 15:03:11 -0300

    Crud de usuarios y middleware de verificacion de token

--- controllers/CrudUsersControllers.js
+++ controllers/CrudUsersControllers.js
@@ -1,11 +1,13 @@
 const User = require("../models/User")
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path")
 
 module.exports = {
     async register(req, res){
-
-        console.log(req.file);
+        
+        // Verificación que no exista el email
 
         const userFind = await User.findOne({email: req.body.email});
 
@@ -13,21 +15,29 @@ module.exports = {
             return res.status(200).send("Ese email ya está en uso");
         }
 
-        const user = new User({...req.body});
+        //Creación de usuario
+
+        const user = await new User({...req.body});
         
         user.password = user.encryptPassword(req.body.password);
+
+        // Gurdado del nombre de la imagen
 
         if(req.file){
             user.image = req.file.filename
         }
         
         if(user){
+            // Guardado de usuario
             await user.save((err, user) => {
                 if(err) return res.status(500).send("Ha ocurrido un error al registrar el usuario");
                 
                 if(!user) return res.status(404).send("Los datos no son correctos");
 
                 if(user){
+
+                    //Generación de token
+
                     const token = jwt.sign({id: user._id}, process.env.JWT_PASSWORD, {
                         expiresIn: 24 * 24 * 60
                     });
@@ -44,17 +54,30 @@ module.exports = {
         const userID = req.params.id;
 
         const { email, name, username, password } = req.body;
+        const newUser = await new User();
 
-        const userFind = await User.findById(userID);
-        if(userFind.comparePassword(password, userFind.password)){
-            return res.status(402).send("La contraseña que estas ")
-        }
+        // Recopilacion de datos para modificar
+
         const userChange = {
             email,
             name,
             username,
-            password: userFind.encryptPassword(password)
+            password: newUser.encryptPassword(password)
         }
+
+        // Si es que tan solo cambia datos como un email o username
+
+        if(req.body && !req.file){
+            await User.findByIdAndUpdate(userID, userChange, {new: true}, (err, user) => {
+                if(err) return res.status(500).send("Ha ocurrido un error al modificar el usuario");
+                    
+                if(!user) return res.status(404).send("Los datos no son correctos");
+    
+                if(user) return res.status(200).send(user);
+            });
+        }
+
+        // Si es que cambia tan solo la imagen
 
         if(!req.body && req.file){
             await User.findByIdAndUpdate(userID, {image: req.file.filename}, {new: true}, (err, user) => {
@@ -66,15 +89,7 @@ module.exports = {
             });
         }
 
-        if(req.body && !req.file){
-            await User.findByIdAndUpdate(userID, userChange, {new: true}, (err, user) => {
-                if(err) return res.status(500).send("Ha ocurrido un error al modificar el usuario");
-                    
-                if(!user) return res.status(404).send("Los datos no son correctos");
-    
-                if(user) return res.status(200).send(user);
-            });
-        }
+        // Si es que cambia ambos
         
         await User.findByIdAndUpdate(userID, {userChange, image: req.file.filename}, {new: true}, (err, user) => {
             if(err) return res.status(500).send("Ha ocurrido un error al modificar el usuario");
@@ -83,5 +98,42 @@ module.exports = {
 
             if(user) return res.status(200).send(user);
         });
+    },
+    async deleteUser(req, res){
+        const userID = req.params.id;
+
+        await User.findByIdAndRemove(userID, async (err, user) => {
+            if(err) return res.status(500).send("Ha ocurrido un error al eliminar el usuario");
+                
+            if(!user) return res.status(404).send("Los datos no son correctos");
+
+            if(user){
+                if(user.image){
+                    fs.unlinkSync(path.join(__dirname, "../uploads/images/" + user.image));
+                }
+
+                return res.status(200).send("Usuario eliminado con éxito")
+            }
+        })
+    },
+    async getUser(req, res){
+        const userID = req.params.id;
+
+        await User.findById(userID, (err, user) => {
+            if(err) return res.status(500).send("Ha ocurrido un error al buscar el usuario");
+                
+            if(!user) return res.status(404).send("Los datos no son correctos o no se encuentra mas ese usuario");
+
+            if(user) return res.status(200).send(user);
+        })
+    },
+    async getUsers(req, res){
+        await User.find((err, user) => {
+            if(err) return res.status(500).send("Ha ocurrido un error al buscar el usuario");
+                
+            if(!user) return res.status(404).send("No se encuentran usuarios registrados");
+
+            if(user) return res.status(200).send(user);
+        })
     }
 }
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const { crudUsers, authentication } = require("../controllers/index");
-const { multer } = require("../middlewares/index");
+const { multer, verifyToken } = require("../middlewares/index");
 
 router.post("/register", multer("images", "image/").single("image"),crudUsers.register);
 router.post("/login", authentication.login);
-router.put("/update/:id", multer("images", "image/").single("image"),crudUsers.changeUser);
+router.put("/update/:id", verifyToken, multer("images", "image/").single("image"),crudUsers.changeUser);
+router.delete("/delete/:id", verifyToken, crudUsers.deleteUser);
+router.get("/user/:id", crudUsers.getUser);
+router.get("/user", crudUsers.getUsers);
 
 module.exports = router;

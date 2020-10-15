5886a34e40bbb0befa64035042c734a5dcd77a34
Thiago Puyelli <thiagopuyelli@pop-os.localdomain>
2020-10-15 16:18:52 -0300

    Crud de secciones

--- controllers/CrudSectionsControllers.js
+++ controllers/CrudSectionsControllers.js
@@ -49,13 +49,50 @@ module.exports = {
 
         for(let i of course.sections){
             if(i.position == position){
-                course.sections.splice(i, 1);
-                for(let index = i; index <= course.sections.length; index++){
-                    course.sections[index].position = index;
+                course.sections.splice(i.position - 1, 1);
+                var indice = i.position;
+                for(let index = indice - 1; index < course.sections.length; index++){
+                    course.sections[index].position = index + 1;
                 }
+                console.log(course);
             }
         }
 
-        console.log(course);
+        await Course.findByIdAndUpdate(course._id, course, {new: true}, (err, courseWithSectionDelete) => {
+            if(err) return res.status(500).send("Error al eliminar sección");
+
+            if(!courseWithSectionDelete) return res.status(404).send("Los datos de la sección no son válidos");
+
+            if(courseWithSectionDelete) return res.status(200).send(courseWithSectionDelete);
+        })
+    },
+    async getSections(req, res){
+        const courseID = req.params.id;
+
+        await Course.findById(courseID, (err, course) => {
+            if(err) return res.status(500).send("Error al buscar el curso");
+
+            if(!course) return res.status(404).send("Los datos del curso no son válidos");
+
+            if(course) return res.status(200).send(course.sections);
+        })
+    },
+    async getSection(req, res){
+        const courseID = req.params.id;
+        const position = req.params.position;
+
+        await Course.findById(courseID, (err, course) => {
+            if(err) return res.status(500).send("Error al buscar el curso");
+
+            if(!course) return res.status(404).send("Los datos del curso no son válidos");
+
+            if(course){
+                for(let i in course.sections){
+                    if(course.sections[i].position == position){
+                        return res.status(200).send(course.sections[i]);
+                    }
+                }
+            }
+        })
     }
 }
--- routes/sectionsRoutes.js
+++ routes/sectionsRoutes.js
@@ -5,5 +5,7 @@ const { crudSections } = require("../controllers/index");
 router.post("/save/:id", verifyToken, findCourse("id"), crudSections.saveSection);
 router.put("/update/:id", verifyToken, findCourse("id"), crudSections.updateSection);
 router.delete("/delete/:courseID/:position", verifyToken, findCourse("courseID"), crudSections.deleteSection);
+router.get("/section/:id", verifyToken, crudSections.getSections);
+router.get("/section/:id/:position", verifyToken, crudSections.getSection)
 
 module.exports = router;
